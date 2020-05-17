541cdb6184c7badb19c2ffee3275c42da13ea1cd
Pri_030 <priyankasingh30@gmail.com>
2020-05-17 13:55:22 -0400

    completed current weather functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,20 +28,18 @@ function displaySearchCity() {
 
 }
 
-
-
 $("#search-btn").on("click", function (event) {
     event.preventDefault();
-
     var city = $("#search-input").val();
-
     if (city === "") {
         return;
     }
+    if (searchCityArray.includes(city)) {
+        displayWeatherInfo(city);
+    };
     searchCityArray.push(city);
 
     $("#search-input").val(" ");
-
 
     displayWeatherInfo(city);
     saveCityInLocalStorage();
@@ -50,7 +48,7 @@ $("#search-btn").on("click", function (event) {
 });
 
 function displayWeatherInfo(city) {
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city},newhampshire,+1&appid=${APIKey}`;
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
     console.log(queryURL);
 
     //We then created an AJAX call
@@ -61,19 +59,59 @@ function displayWeatherInfo(city) {
 
         console.log(response);
         $(".city").text(response.name);
-        $(".wind").text("Wind Speed:" + " " + response.wind.speed);
-        $(".humidity").text("Humidity:" + " " + response.main.humidity);
+        var iconCode = response.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        $(".icon").attr("src", iconURL);
+        var currentDay = moment().format("MMMM Do YYYY");
+        $(".current-day").text("(" + currentDay + ")");
+        $(".wind").text("Wind Speed:" + " " + response.wind.speed + " " + "MPH");
+        $(".humidity").text("Humidity:" + " " + response.main.humidity + " " + "%");
         var Temp = response.main.temp
         var F = (Temp - 273.15) * 1.80 + 32;
-        $(".temp").text("Temperature:" + " " + F);
+        $(".temp").text("Temperature:" + " " + F + " " + "°F");
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        uvQueryURL = `http://api.openweathermap.org/data/2.5/uvi/forecast?&appid=${APIKey}&lat=${lat}&lon=${lon}`;
+        console.log(uvQueryURL);
 
-    });
-}
+        //This is nested ajax request that gets the UV index but uses longitude and latitude from the previous ajax request to do so.
+        $.ajax({
+            url: uvQueryURL,
+            method: "GET"
+        }).then(function (response) {
+            var uvIndex = response[0].value;
+            console.log(uvIndex);
+
+            $("#uv-value").text("UV Index:" + uvIndex);
+            if (uvIndex >= 9) {
+                $("#uv-value").addClass("red");
+            }
+            else if (uvIndex > 4) {
+                $("#uv-value").addClass("orange");
+            }
+            else {
+                $("#uv-value").addClass("green");
+            }
+        });
+    })
+
+    //This ajax request collects weather data for the next 5 days (specifically it is grabbing the stays from noon, as opposed to every few hours)
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+    }).then(function (response) {
+
+
+
+
+
+    }
+};
+
+
 
 function saveCityInLocalStorage() {
-
     localStorage.setItem("history", JSON.stringify(searchCityArray));
-
 }
 
 $(document).on("click", ".city-btn", function () {
