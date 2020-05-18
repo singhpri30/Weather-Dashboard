c360d660e7debb79b3d5198a07909fa7f81dd4a0
Pri_030 <priyankasingh30@gmail.com>
2020-05-17 22:08:13 -0400

    added 5 day forecast functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-
 var searchCityArray = [];
 var APIKey = '5e7733603521cee54360e9a63ebcc0a7';
+
 
 getCityFromLocalStorage();
 
@@ -19,8 +19,8 @@ function displaySearchCity() {
         var cityName = searchCityArray[i];
         console.log(cityName);
 
-        var cityElement = $("<button>");
-        cityElement.addClass("city-btn btn btn-primary btn-lg");
+        var cityElement = $("<p>").addClass("card-text city-btn btn btn-primary btn-lg");
+        //cityElement.addClass("city-btn btn btn-primary btn-lg");
         cityElement.attr("data-name", cityName);
         cityElement.text(cityName);
         $("#search-result-data").prepend(cityElement);
@@ -30,16 +30,18 @@ function displaySearchCity() {
 
 $("#search-btn").on("click", function (event) {
     event.preventDefault();
+
     var city = $("#search-input").val();
-    if (city === "") {
-        return;
+    // if (city === "") {
+    //     return;
+    // }
+    if (searchCityArray.indexOf(city) === -1) {
+
+        searchCityArray.push(city);
     }
-    if (searchCityArray.includes(city)) {
-        displayWeatherInfo(city);
-    };
-    searchCityArray.push(city);
 
     $("#search-input").val(" ");
+
 
     displayWeatherInfo(city);
     saveCityInLocalStorage();
@@ -48,6 +50,8 @@ $("#search-btn").on("click", function (event) {
 });
 
 function displayWeatherInfo(city) {
+    $("#city-section").removeClass("d-none");
+
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
     console.log(queryURL);
 
@@ -75,6 +79,7 @@ function displayWeatherInfo(city) {
         console.log(uvQueryURL);
 
         //This is nested ajax request that gets the UV index but uses longitude and latitude from the previous ajax request to do so.
+
         $.ajax({
             url: uvQueryURL,
             method: "GET"
@@ -96,16 +101,39 @@ function displayWeatherInfo(city) {
     })
 
     //This ajax request collects weather data for the next 5 days (specifically it is grabbing the stays from noon, as opposed to every few hours)
+    var queryURL2 = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`;
     $.ajax({
         url: queryURL2,
         method: "GET"
     }).then(function (response) {
+        $(".forecast-list").empty();
+        var forecastTimes = response.list;
+        for (i = 0; i < forecastTimes.length; i++) {
+
+            if (forecastTimes[i].dt_txt[12] === "2") {
+
+                var dateValue = forecastTimes[i].dt_txt;
+                var dt = moment(dateValue, moment.ISO_8601).format('MM/DD/YYYY'); //changing the date format
+                //var dateEl = $("<p>").text(dt);
+
+                var iconCode = forecastTimes[i].weather[0].icon;
+                var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+                //var iconImageEl = $("<img>").attr("src", iconURL);
+                var forecastHum = forecastTimes[i].main.humidity;
+                //humidityEl.text("Humidity:" + " " + forecastTimes[i].main.humidity + " " + "%");
+                var Temp = forecastTimes[i].main.temp
+                var F = (Temp - 273.15) * 1.80 + 32;
 
 
-
-
-
-    }
+                $(".forecast-list").append("<div class='my-3 pb-3 col-md-2 col-lg-2 forecast-day'>" +
+                    "<h5>" + dt + "<h5>" +
+                    "<img class='ficon' src=" + iconURL + " alt='Weather icon'>" +
+                    "<div>Temp: " + F.toFixed(1) + " °F" +
+                    "</div><div>Humidity: " + forecastHum +
+                    "%</div></div></div>");
+            }
+        }
+    });
 };
 
 
